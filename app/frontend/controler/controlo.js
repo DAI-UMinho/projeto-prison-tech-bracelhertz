771d57ae0a55e7037a0e99556af815830bc84c43
JoaoTeixeira89218 <joao07um@gmail.com>
2020-03-12 23:14:42 +0000

    Adicionado todo o frontend ate ao momento

---
+++ app/frontend/controler/controlo.js
@@ -0,0 +1,16 @@
+export function pulsacoes() {
+
+    //criação da demonstração de resultados recebidos
+
+    //envia a para a pagina
+
+    //chama a função fetchAsync()
+
+    //----------------------------------------------------------------------------------------------
+
+
+    var array1 = [1, 10, 2, 21]; 
+    array1.sort();
+    console.log(array1);
+    console.log("ola");
+}
---
+++ app/frontend/js/sb-admin-2.js
@@ -0,0 +1,126 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Toggle the side navigation
+  $("#sidebarToggle, #sidebarToggleTop").on('click', function(e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    if ($(".sidebar").hasClass("toggled")) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+  // Close any open menu accordions when window is resized below 768px
+  $(window).resize(function() {
+    if ($(window).width() < 768) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+  });
+
+  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
+    if ($(window).width() > 768) {
+      var e0 = e.originalEvent,
+        delta = e0.wheelDelta || -e0.detail;
+      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+      e.preventDefault();
+    }
+  });
+
+  // Scroll to top button appear
+  $(document).on('scroll', function() {
+    var scrollDistance = $(this).scrollTop();
+    if (scrollDistance > 100) {
+      $('.scroll-to-top').fadeIn();
+    } else {
+      $('.scroll-to-top').fadeOut();
+    }
+  });
+
+  // Smooth scrolling using jQuery easing
+  $(document).on('click', 'a.scroll-to-top', function(e) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top)
+    }, 1000, 'easeInOutExpo');
+    e.preventDefault();
+  });
+
+})(jQuery); // End of use strict
+
+
+$(".myFilter").keyup(function () {
+  val = $.trim(this.value);
+  if (val == "") {
+    $('.myfilter').show();
+  } else {
+    $('.myfilter').hide();
+    $(".myfilter:contains(" + val + ")").show();
+  }
+});
+
+$(function(){   
+
+  $(".myFilter").keyup(function(){       
+
+      var searchText = $(this).val().toUpperCase();
+
+      $('.myfilter').each(function(){
+
+          var currentLiText = $(this).text().toUpperCase(),
+              showCurrentLi = currentLiText.indexOf(searchText) !== -1;
+    
+          $(this).toggle(showCurrentLi);
+
+      });
+
+  });
+
+});
+
+
+function startTime() {
+  var today = new Date();
+  var h = today.getHours();
+  var m = today.getMinutes();
+  var s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('txt').innerHTML =
+  h + ":" + m +"h";
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var shi;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (shi = 0; shi < slides.length; shi++) {
+    slides[shi].style.display = "none";
+  }
+  for (shi = 0; shi < dots.length; shi++) {
+    dots[shi].className = dots[shi].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+//------------------------------------------------------Novo Codigo ---------------------------------------------------------------
+
