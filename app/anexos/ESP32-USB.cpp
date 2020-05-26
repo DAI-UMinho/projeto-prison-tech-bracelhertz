f2841bf92b76b8b799b22da25987789fdb400fd4
ruigomes99 <yruigomes99@gmail.com>
2020-05-26 23:18:53 +0100

    update

--- app/anexos/ESP32-USB.cpp
+++ app/anexos/ESP32-USB.cpp
@@ -1,9 +1,39 @@
-void setup() {
-  Serial.begin(115200);
-}
+    int UpperThreshold = 518;
+    int LowerThreshold = 490;
+    int reading = 0;
+    int BPM = 0;
+    bool IgnoreReading = false;
+    bool FirstPulseDetected = false;
+    unsigned long FirstPulseTime = 0;
+    unsigned long SecondPulseTime = 0;
+    unsigned long PulseInterval = 0;
+    
+    void setup(){
+      Serial.begin(115200);
+    }
 
-void loop() {
-  int Signal = analogRead(A0);
-  Serial.println(Signal);
-  delay(500);
-}
+    void loop(){
+
+      reading = analogRead(A0); 
+      if(reading > UpperThreshold && IgnoreReading == false){
+        if(FirstPulseDetected == false){
+          FirstPulseTime = millis();
+          FirstPulseDetected = true;
+        }
+        else{
+          SecondPulseTime = millis();
+          PulseInterval = SecondPulseTime - FirstPulseTime;
+          FirstPulseTime = SecondPulseTime;
+        }
+        IgnoreReading = true;
+      }
+      if(reading < LowerThreshold){
+        IgnoreReading = false;
+      }  
+
+       // uncomment these lines in case you want to view the various values in the console.....
+      Serial.print(reading);
+      Serial.println("");
+      Serial.flush();
+      delay(1000);
+    }
